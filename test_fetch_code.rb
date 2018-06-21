d606c124575a7ab7659c6a3360a7156c15eb278a
Eponick <mikehuntwife@gmail.com>
2018-06-21 19:07:13 -0400

    Test fetch returns string, fail

--- test_fetch_code.rb
+++ test_fetch_code.rb
@@ -5,4 +5,8 @@ class TestFetchCode < Minitest::Test
   def test_assert_1_equals_1
     assert_equal(1, 1)
   end
+
+  def test_assert_returns_string
+    assert_equal(String, fetch_code('').class)
+  end
 end
